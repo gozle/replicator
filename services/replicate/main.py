2375f238bccc83b05d667359840d99f6b5e2fba2
begench <begenchallakov070@gmail.com>
2023-10-25 21:12:46 +0500

    done replicate

--- services/replicate/main.py
+++ services/replicate/main.py
@@ -23,12 +23,23 @@ def replicate():
     replications = Replication.objects.filter(replicate=True, node=this_node)
     files = File.objects.filter(source__in=[repl.source for repl in replications]).exclude(replicas__in=[this_node])
     for file in files:
-        print(file.path)
+        status = save_file(file)
+        if status:
+            file.replicas.add(this_node)
 
 
 def save_file(file):
-    response = requests.get(file.url, stream=True)
+    url = file.url
+    try:
+        response = requests.get(file.url, stream=True)
+    except Exception as e:
+        print('[ERROR] static server not responds', url)
+        return None
     path = get_save_path(file)
     with open(path, mode="wb") as f:
         for chunk in response.iter_content(chunk_size=10 * 1024):
             f.write(chunk)
+
+
+if __name__ == '__main__':
+    worker()
